edb5025665f69d55cc029f8837924ce24f9c38d1
Bruna Colin Baldin e Silva <brunacbaldin@gmail.com>
2022-11-16 13:44:22 -0300

    date select

--- backend/src/main/java/com/brunas/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/brunas/dsmeta/services/SaleService.java
@@ -2,8 +2,13 @@ package com.brunas.dsmeta.services;
 
 import com.brunas.dsmeta.entities.Sale;
 import com.brunas.dsmeta.repositories.SaleRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 
 @Service
@@ -15,7 +20,13 @@ public class SaleService {
         this.repository = repository;
     }
 
-    public List<Sale> findSales() {
-        return repository.findAll();
+    public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+
+        LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+        LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+        LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate); //se maxDate igual a "String vázio" ? faça se verdadeiro : faça se falso;
+
+        return repository.findSales(min, max, pageable);
     }
 }
