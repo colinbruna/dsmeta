d97f30ff410fceb78278bc09d52d682d05781344
Bruna Colin Baldin e Silva <brunacbaldin@gmail.com>
2022-11-18 12:36:02 -0300

    Axios, useEffect first request

--- backend/src/main/java/com/brunas/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/brunas/dsmeta/services/SmsService.java
@@ -8,8 +8,6 @@ import com.twilio.type.PhoneNumber;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.text.DecimalFormat;
-
 @Service
 public class SmsService {
 
